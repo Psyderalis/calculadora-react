9e64e34a1885f39f2f457a4aa7ef99db5fdbb3d6
Melanie Tolvett S <melanietolvett@gmail.com>
2023-03-14 19:58:15 -0300

    componente pantalla y sus estilos

--- src/componentes/Pantalla.jsx
+++ src/componentes/Pantalla.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../stylesheets/Pantalla.css';
 
 //Se suele usar este formato (arrow function) para componentes que son más sencillos y no involucran más funciones
 const Pantalla = ({ input }) => (
