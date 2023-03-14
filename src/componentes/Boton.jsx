c761a9643623839f27e517291c6ff47a00c9c0fa
Melanie Tolvett S <melanietolvett@gmail.com>
2023-03-14 20:38:43 -0300

    logica al de manejar clics

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -8,9 +8,11 @@ function Boton(props) {
   };
 
   return (
-    <div 
-    //le vamos a asignar dos clases al boton, en caso de que sea un boton operador (no numero, punto o igual)
-    className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}> 
+    <div
+      //le vamos a asignar dos clases al boton, en caso de que sea un boton operador (no numero, punto o igual)
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}
+      //onClick espera que le pasemos una funcion, por eso se envuelve como funcion flecha, sino la funcion props.manejarClic(props.children) se traduce como el valor de retorno (string) y arroja error
+      onClick={() => props.manejarClic(props.children)}>
       {props.children}
     </div>
   );
